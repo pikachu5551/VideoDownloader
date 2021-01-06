6a9cfad5c655c2fb9358a50bf5ec8122e3e8db68
pikachu551 <nikelic.marvalis@gmail.com>
2021-01-07 01:03:26 +0900

    youtube-dl.exeがディレクトリに存在しているかを確認するようにした

--- VideoDownloader/MainWindow.xaml.cs
+++ VideoDownloader/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,30 @@ namespace VideoDownloader
     /// </summary>
     public partial class MainWindow : Window
     {
+        /* youtube-dlのファイルパスを変数に代入
+           @はヒアドキュメントという物の為に付けている
+           エスケープシーケンスが不要になる */
+        public string YoutubeDlFilePath = Environment.CurrentDirectory + @"\youtube-dl.exe";
         public MainWindow()
         {
             InitializeComponent();
+
+            // ディレクトリにyoutube-dlが存在しないと知らせアプリを終了
+            if (!File.Exists(YoutubeDlFilePath))
+            {
+                MessageBox.Show("次のフォルダにyoutube-dl.exeが存在しません\n" + Environment.CurrentDirectory);
+                Environment.Exit(1);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            outputLog.Text = YoutubeDlFilePath;
+            /*if (!(downloadURL.Text == null))
+            {
+                // 第一引数のプロセスを起動
+                Process.Start(, downloadURL.Text);
+            }*/
         }
     }
 }
